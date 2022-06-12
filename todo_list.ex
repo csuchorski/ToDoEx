3d93b47bb9e3e9671ffe0fe11ed2790bf20993dc
csuchorski <cezary.suchorski@wp.pl>
2022-06-12 22:27:08 +0200

    Created draft of methods that will be implemented

---
+++ todo_list.ex
@@ -0,0 +1,41 @@
+defmodule ToDoList do
+  def start() do
+    # Runs until users gives the command to quit
+  end
+
+  def read_file(filename) do
+    # Opens a file
+  end
+
+  def parse_file(content) do
+    # Parses the CSV format
+  end
+
+  def parse_lines(titles, lines) do
+    # Parses separate lines into fields
+  end
+
+  def show_tasks() do
+    # Lists all tasks
+  end
+
+  def create_task() do
+    # Adds new tasks
+  end
+
+  def delete_task() do
+    # Delete specific task
+  end
+
+  def update_task() do
+    # Update specific task
+  end
+
+  def save_tasks() do
+    # Save changes to the file
+  end
+
+  def show_help do
+    # Print all commands
+  end
+end
