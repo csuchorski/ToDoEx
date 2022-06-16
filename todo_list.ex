d9ddca44345e65d38b2cef6aa7646ed1c13e36a3
csuchorski <cezary.suchorski@wp.pl>
2022-06-16 19:47:41 +0200

    Implemented help command and read_file, parse_lines splits csv into list of lines

--- todo_list.ex
+++ todo_list.ex
@@ -1,14 +1,23 @@
 defmodule ToDoList do
   def start() do
-    # Runs until users gives the command to quit
+    filename = IO.gets("Input file name: ") |> String.trim()
+    read_file(filename)
+    # show_help()
   end
 
   def read_file(filename) do
     # Opens a file
+    case File.read(filename) do
+      {:ok, content} ->
+        parse_file(content |> String.replace_prefix("\uFEFF", ""))
+
+      {:error, error} ->
+        IO.puts("Error while loading file: #{error}")
+    end
   end
 
   def parse_file(content) do
-    # Parses the CSV format
+    lines = String.split(content, ~r(\r\n|\r|\n))
   end
 
   def parse_lines(titles, lines) do
@@ -35,7 +44,43 @@ defmodule ToDoList do
     # Save changes to the file
   end
 
-  def show_help do
-    # Print all commands
+  def show_help() do
+    IO.puts(~S"Available commands:\n
+        c ->create new task\n
+        d ->delete existing task
+        l ->list all existing tasks
+        s ->save tasks to a file
+        h ->show all commands
+        ")
+    get_command()
+  end
+
+  def get_command() do
+    command = IO.gets("Input command: ") |> String.trim()
+
+    case command do
+      "c" ->
+        create_task()
+
+      "d" ->
+        delete_task()
+
+      "l" ->
+        show_tasks()
+
+      "u" ->
+        update_task()
+
+      "s" ->
+        save_tasks()
+
+      "h" ->
+        show_help()
+
+      _ ->
+        IO.puts("Please input correct command.\n")
+        show_help()
+        get_command()
+    end
   end
 end
